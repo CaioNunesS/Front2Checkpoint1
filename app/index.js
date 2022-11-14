37046bde29690e432b257f0f36cf3145e4054ae6
pss-ctd <pedro.simoes.soares@gmail.com>
2022-11-14 20:03:56 -0300

    ... make constructorCard functional with form

--- app/index.js
+++ app/index.js
@@ -9,18 +9,19 @@ let arrayCards = [];
 ///////////////////////////////////////////////////////////////////////////////////////
 ////// tring to create a html template for the card using document.createElement() method//////
 
-// create card html template
-// const card = document.createElement('li');
-// card.classList.add('card');
-// card.innerHTML = `
+// // create card html template
+// const card_template = document.createElement('li');
+// card_template.classList.add('card');
+// card_template.innerHTML = `
 //     <img class="card-image" src="./assets/eggsB.jpg" alt="image">
 //     <h2 class="card-title">Title: card-0000</h2>
 //     <p class="card-description">Description: Este é um card de exemplo para ser clonado</p>
-//     <div class="card-footer">
-//         <p class="card-date">Date: 00/00/0000</p>
-//         <p class="card-time">Time: 00:00:00</p>
-//     </div>
 // `;
+// // set attribute class and id to the card template
+// card_template.setAttribute('class', 'card');
+// card_template.setAttribute('id', 'card-0000');
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
@@ -33,7 +34,27 @@ const list = document.querySelector('#cards-ulist');
 
 // add a new card item to the list by cloning the card element, adding a new id and appending it to the list; edit the new card item title and description for testing
 const addCard = () => {
-    const newCard = card.cloneNode(true);
+
+    // create card html template
+    const card_template = document.createElement('li');
+    card_template.classList.add('card');
+    card_template.innerHTML = `
+        <img class="card-image" src="./assets/eggsB.jpg" alt="image">
+        <h2 class="card-title">Title: card-0000</h2>
+        <p class="card-description">Description: Este é um card de exemplo para ser clonado</p>
+    `;
+    // set attribute class and id to the card template
+    card_template.setAttribute('class', 'card');
+    card_template.setAttribute('id', 'card-0000');
+    // set attribute so the card is invisible
+    card_template.setAttribute('style', 'display: none;');
+
+    list.appendChild(card_template);
+
+
+    const newCard = card_template.cloneNode(true);
+    // set newCard attribute to be visible
+    newCard.setAttribute('style', 'display: block;');
     // set newCard id by incrementing the last card id number, number format is 0000
     newCard.id = 'card-' + (parseInt(list.firstElementChild.id.slice(5)) + 1).toString().padStart(4, '0');
     console.log(newCard.id);
@@ -47,10 +68,22 @@ const addCard = () => {
     // list.appendChild(newCard);
     list.insertBefore(newCard, list.firstElementChild);
 
+
+    // get all cards with attribute style = display: none;
+    const cards_DisplayNone = document.querySelectorAll('[style="display: none;"]');
+    // remove all cards with attribute style = display: none;
+    cards_DisplayNone.forEach(card => card.remove());
+
 }
 // test addCard function
 addCard();
 addCard();
+addCard();
+addCard();
+addCard();
+
+
+
 
 
 

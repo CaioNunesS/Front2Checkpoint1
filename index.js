24e11971a0174d8cdfc1d7add7bc8478638d1573
pss-ctd <pedro.simoes.soares@gmail.com>
2022-11-15 14:52:40 -0300

    html edited; insertBefore needs fixing yet

--- index.js
+++ index.js
@@ -12,32 +12,33 @@ btnEnviar.addEventListener("click", function(evento) {
     let imagem = document.getElementById("urlimagem").value;
     let descricao = document.getElementById("descricao").value;
     
-    
-let novaReceita = {
-    img: imagem,
-    titulo: titulo,
-    descricao: descricao
+    let novaReceita = {
+        img: imagem,
+        titulo: titulo,
+        descricao: descricao
+    }
 
-}
+    addCard()
 
-addCard()
+    card.innerHTML = `
 
-card.innerHTML = `
+        
+        <img id="imagem" class="card-img-top" src=${novaReceita.img} alt="">
+        <div class="card-body">
+            <h5 class="card-title">${novaReceita.titulo}</h5>
+            <p class="card-text" >${novaReceita.descricao}</p>
+            <button type="button" class="btn btn-primary btn-primary-custom" data-bs-toggle="modal" data-bs-target="#Bobodecamaraomoldal">Ver Receita</button>
+        </div>
 
-    
-      <img id="imagem" class="card-img-top" src=${novaReceita.img} alt="">
-      <div class="card-body">
-        <h5 class="card-title">${novaReceita.titulo}</h5>
-        <p class="card-text" >${novaReceita.descricao}</p>
-        <button type="button" class="btn btn-primary btn-primary-custom" data-bs-toggle="modal" data-bs-target="#Bobodecamaraomoldal">Ver Receita</button>
-      </div>
+    `
+    // cards.appendChild(card)
+    // appendChild as first child
+    cards.insertBefore(card, cards.firstChild)
 
-`
-cards.appendChild(card)
-
-clearInput()
+    clearInput()
 
 });
+
 
 let addCard = () => {
     const newCard = card.cloneNode(true);
@@ -45,23 +46,35 @@ let addCard = () => {
 
 }
 
-// cards.appendChild(newCard);
-
-
 function clearInput() {
     document.getElementById("formId").reset();
 }
 
-// function checkUrl(string) {
-//     try {
-//      let url = new URL(string)
-//      console.log("Valid URL!")
-//    } catch(err) {
-//        console.log("Invalid URL!")
-//    }
-//  }
-// checkUrl(imagem)
 
 
 
- 
+
+
+
+let titulo = document.querySelector("#titulo");
+let urlimagem = document.querySelector("#urlimagem");
+let descricao = document.querySelector("#descricao");
+
+
+
+// // insert date in the input descricao
+// let data = new Date();
+// let dia = data.getDate();
+// let mes = data.getMonth() + 1;
+// let ano = data.getFullYear();
+// let dataAtual = dia + "/" + mes + "/" + ano;
+// descricao.value = dataAtual;
+
+// // insert text example in the input titulo
+// let tituloExemplo = "Exemplo: Bolo de cenoura";
+// titulo.value = tituloExemplo;
+
+// // insert image link example in the input urlimagem
+// let urlExemplo = "https://media.istockphoto.com/id/1313708601/pt/foto/carrot-cake-with-chocolate-icing-brazilian-cake.jpg?s=612x612&w=0&k=20&c=XoZZgYXNyEcoKkY0hAEzcOYT3wqlAm5rCCK_9qV1naQ=";
+// urlimagem.value = urlExemplo;
+
